696319ddc6760b0116a19a2cd63a27907144468b
abuhurraira <36420917+abuhurraira@users.noreply.github.com>
2025-03-11 23:23:34 +1300

    Added Redux, Integrated Jobs API, added Loaders

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
+import { Providers } from './Providers'; // Import Providers from Providers.js
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Header from './components/layout/Header'; 
-
+import Header from './components/layout/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,9 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <Providers> {/* Using Providers here */}
+          <Header />
+          {children}
+        </Providers>
       </body>
     </html>
   );
-} 
+}
